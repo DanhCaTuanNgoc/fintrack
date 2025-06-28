9a09fddcf1a2f4b94118983630c7ecb727bf598c
Nguyễn Ngọc Tuấn <127586212+DanhCaTuanNgoc@users.noreply.github.com>
2025-06-28 15:11:22 +0700

    refactor: clean up localization exports and remove unused extension utility

--- lib/utils/extendsion.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension ColorOpacityFix on Color {
-  Color withAlphaOpacity(double opacity) {
-    return withValues(alpha: (opacity * 255).toDouble());
-  }
-}
